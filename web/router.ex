9309174baf616cb64e4196fb9db3e93dd7abd90d
Sten Karlson <sten.karlson@gmail.com>
2017-12-29 00:41:54 +0200

    feat: create new topic

--- web/router.ex
+++ web/router.ex
@@ -15,8 +15,9 @@ defmodule Discuss.Router do
 
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
-
     get "/", PageController, :index
+    get "/topics/new", TopicController, :new
+    post "/topics", TopicController, :create
   end
 
   # Other scopes may use custom stacks.
